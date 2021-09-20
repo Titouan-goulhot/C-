a0666a02139c3bac80bf8d6f6c989055e2be6e81
Titouan Goulhot <73955271+Titouan-goulhot@users.noreply.github.com>
2021-09-20 17:57:03 +0200

    Add files via upload

--- HelloWorld/HelloWorld/Message.cs
+++ HelloWorld/HelloWorld/Message.cs
@@ -31,7 +31,39 @@ namespace HelloWorld
         // Getter & Setter
         public DateTime Dt
         {
-            get { return dt; }
+            get
+            {
+                
+                //Affichage le matin en semaine
+                if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday && dt.Hour >= 9 && dt.Hour <= 13)
+                {
+                    return ("Bonjour ! " + Environment.UserName);
+                }
+
+                //Affichage l'après-midi en semaine
+                else if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday && dt.Hour >= 13 && dt.Hour <= 18)
+                {
+                    return "Bon Après-Midi ! " + Environment.UserName;
+                }
+
+                //Affichage du soir en semaine
+                else if (dt.DayOfWeek != DayOfWeek.Saturday && dt.DayOfWeek != DayOfWeek.Sunday && dt.Hour >= 18 && dt.Hour <= 9)
+                {
+                    return ("Bonsoir ! " + Environment.UserName;
+                }
+
+
+                //Affichage du Week-End le vendredi après 18h && Lundi avant 9h
+                else if (dt.DayOfWeek == DayOfWeek.Friday && dt.Hour >= 18 && dt.DayOfWeek == DayOfWeek.Monday && dt.Hour <= 9)
+                {
+                    return ("Bon Week-End ! " + Environment.UserName);
+
+                } //Affichage du Week-End le Samedi && Dimanche
+                else if (dt.DayOfWeek == DayOfWeek.Saturday && dt.DayOfWeek == DayOfWeek.Sunday)
+                {
+                    return ("Bon Week-End ! " + Environment.UserName);
+                };
+            }
         }
 
         public string HelloMessage
