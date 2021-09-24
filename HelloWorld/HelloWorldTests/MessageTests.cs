6f27156178571bbf615ed6de2f0b016b84fdd386
Titouan.goulhot <titouan.goulhot-bonichon@le-campus-numerique.fr>
2021-09-24 16:00:24 +0200

    fin de l'itération 2

--- HelloWorld/HelloWorldTests/MessageTests.cs
+++ HelloWorld/HelloWorldTests/MessageTests.cs
@@ -12,52 +12,62 @@ namespace HelloWorld.Tests
     public class MessageTests
     {
         [TestMethod()]
-        public void MessageOfTheDayTest()
+        public void MessageOfTheNight()
         {
 
-
             //En semaine le Soir
-            Message message = new Message( new FakeMyDateTime(DayOfWeek.Monday, 19));
+            Message message = new Message(new FakeMyDateTime(DayOfWeek.Monday, 19));
 
             message.MessageOfTheDay();
 
             Assert.AreEqual(message.HelloMessage, "Bonsoir ! titouan.goulhot");
 
+        }
 
+        [TestMethod()]
+        public void MessageOfTheAfternoon()
+        { 
             // En semaine l'après-midi
             Message message1 = new Message(new FakeMyDateTime(DayOfWeek.Monday, 15));
 
             message1.MessageOfTheDay();
 
             Assert.AreEqual(message1.HelloMessage, "Bon Après-Midi ! titouan.goulhot");
+        }
 
+        [TestMethod()]
+        public void MessageOfTheWeekEnd()
+        {
 
-            // En Week-End
-            Message message2 = new Message(new FakeMyDateTime(DayOfWeek.Sunday, 6));
+        // En Week-End
+        Message message2 = new Message(new FakeMyDateTime(DayOfWeek.Sunday, 6));
 
             message2.MessageOfTheDay();
 
             Assert.AreEqual(message2.HelloMessage, "Bon Week-End ! titouan.goulhot");
+        }
 
+        [TestMethod()]
+        public void MessageOfTheMondayMorning()
+        {
 
-            //Le Lundi avant "le Matin"
-            Message message3 = new Message(new FakeMyDateTime(DayOfWeek.Monday, 6));
+        //Le Lundi avant "le Matin"
+        Message message3 = new Message(new FakeMyDateTime(DayOfWeek.Monday, 6));
 
             message3.MessageOfTheDay();
 
             Assert.AreEqual(message3.HelloMessage, "Bon Week-End ! titouan.goulhot");
+        }
 
-
-
-            //Le vendredi soir
-            Message message4 = new Message(new FakeMyDateTime(DayOfWeek.Friday, 20));
+        [TestMethod()]
+        public void MessageOfTheFridayNight()
+        {
+        //Le vendredi soir
+        Message message4 = new Message(new FakeMyDateTime(DayOfWeek.Friday, 20));
 
             message4.MessageOfTheDay();
 
             Assert.AreEqual(message4.HelloMessage, "Bon Week-End ! titouan.goulhot");
-
-
-
         }
     }
 }
